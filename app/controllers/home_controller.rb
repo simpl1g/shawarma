538d7709817febeafb240ee6043ae0fd6a209eef
Konstantin Ilchenko <konstantin@ilchenko.by>
2017-10-28 10:22:18 +0300

    Add displaying of active and future deliveries

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @active_delivery = Delivery.where("start_at < now() and close_at > now()").first
+    @future_delivery = Delivery.where("start_at >= now()").first
   end
 end
