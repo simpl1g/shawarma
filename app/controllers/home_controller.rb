f214f88a5e99cd0ad3f5ee78a25a44760e8e14d8
Konstantin Ilchenko <konstantin@ilchenko.by>
2017-11-02 20:06:51 +0300

    Fix home if not signed in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @future_delivery = Delivery.next
-    @orders = current_user.active_orders.includes(order_items: :store_item)
+    @orders = user_signed_in? ? current_user.active_orders.includes(order_items: :store_item) : Order.none
   end
 end
